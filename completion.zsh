7daf34c925ed9f9d4064e4a992cb00785825df43
John Schmidt <john.schmidt.h@gmail.com>
2021-12-12 11:45:52 +0100

    Use vi bindings to navigate tab complete menu

--- completion.zsh
+++ completion.zsh
@@ -30,3 +30,7 @@ zstyle ':completion:*' cache-path ~/.zcompcache
 zstyle '*' single-ignored show
 
 bindkey -M menuselect '^M' .accept-line
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
